5d014f5aea511f5aa739aa0b5c1a1597e59a006b
zyv42 <zyv42@sdf.org>
2020-03-07 23:35:41 +0200

    add User class to the account-service module

---
+++ account-service/src/main/java/com/eugenarium/account/persistence/domain/User.java
@@ -0,0 +1,41 @@
+package com.eugenarium.account.persistence.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class User {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
