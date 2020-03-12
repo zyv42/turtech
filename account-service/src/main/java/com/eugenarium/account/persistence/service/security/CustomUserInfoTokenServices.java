b8b08e0d060ceafd6130548af0177948640e5fa9
zyv42 <zyv42@sdf.org>
2020-03-12 18:44:07 +0200

    update CustomUserInfoTokenServices.java at account-service

--- account-service/src/main/java/com/eugenarium/account/persistence/service/security/CustomUserInfoTokenServices.java
+++ account-service/src/main/java/com/eugenarium/account/persistence/service/security/CustomUserInfoTokenServices.java
@@ -4,11 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.FixedAuthoritiesExtractor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.BaseOAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+import java.util.*;
 
 public class CustomUserInfoTokenServices implements ResourceServerTokenServices {
 
@@ -41,9 +50,72 @@ public class CustomUserInfoTokenServices implements ResourceServerTokenServices 
     }
 
     @Override
-    public OAuth2Authentication loadAuthentication(String accessToken) {
-        // TODO
-        return null;
+    public OAuth2Authentication loadAuthentication(String accessToken)
+        throws AuthenticationException, InvalidTokenException {
+
+        Map<String, Object> map = getMap(this.userInfoEndpointUrl, accessToken);
+        if (map.containsKey("error")) {
+            this.log.debug("userinfo returned error: " + map.get("error"));
+            throw new InvalidTokenException(accessToken);
+        }
+
+        return extractAuthentication(map);
+    }
+
+    private OAuth2Authentication extractAuthentication(Map<String, Object> map) {
+        Object principal = getPrincipal(map);
+        OAuth2Request request = getRequest(map);
+        List<GrantedAuthority> authorities = this.authoritiesExtractor.extractAuthorities(map);
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(principal, "N/A",
+                 authorities);
+        token.setDetails(map);
+
+        return new OAuth2Authentication(request, token);
+    }
+
+    private Object getPrincipal(Map<String, Object> map) {
+        for (String key : PRINCIPAL_KEYS) {
+            if (map.containsKey(key)) {
+                return map.get(key);
+            }
+        }
+
+        return "unknown";
+    }
+
+    @SuppressWarnings({"unchecked"})
+    private OAuth2Request getRequest(Map<String, Object> map) {
+        Map<String, Object> request = (Map<String, Object>) map.get("oauth2Request");
+
+        String clientId = (String) request.get("clientId");
+        Set<String> scope = new LinkedHashSet<>(request.containsKey("scope") ?
+                (Collection<String>) request.get("scope") : Collections.<String>emptySet());
+
+        return new OAuth2Request(null, clientId, null, true, new HashSet<>(scope),
+                null, null, null, null);
+    }
+
+    @SuppressWarnings({"unchecked"})
+    private Map<String, Object> getMap(String path, String accessToken) {
+        this.log.debug("getting user info from: " + path);
+        try {
+            OAuth2RestOperations restTemplate = this.restTemplate;
+            if (restTemplate == null) {
+                BaseOAuth2ProtectedResourceDetails resource = new BaseOAuth2ProtectedResourceDetails();
+                resource.setClientId(this.clientId);
+                restTemplate = new OAuth2RestTemplate(resource);
+            }
+            OAuth2AccessToken existingToken = restTemplate.getOAuth2ClientContext().getAccessToken();
+            if (existingToken == null || !accessToken.equals(existingToken.getValue())) {
+                DefaultOAuth2AccessToken token = new DefaultOAuth2AccessToken(accessToken);
+                token.setTokenType(this.tokenType);
+                restTemplate.getOAuth2ClientContext().setAccessToken(token);
+            }
+            return restTemplate.getForEntity(path, Map.class).getBody();
+        } catch (Exception e) {
+            this.log.info("could not fetch user details: " + e.getClass() + ", " + e.getMessage());
+            return Collections.<String,Object>singletonMap("error", "could not fetch user details");
+        }
     }
 
     @Override
