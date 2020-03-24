c4688e7b9defad2438647dd1db8620c5ed3dcfd9
zyv42 <zyv42@sdf.org>
2020-03-24 21:22:45 +0200

    update Login.js component at the frontend-client module

--- frontend-client/src/components/Security/Login.js
+++ frontend-client/src/components/Security/Login.js
@@ -1,48 +1,105 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import { login } from "../../actions/securityActions";
 
 class Login extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: "",
+            errors: {}
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        if (this.props.security.validToken) {
+            this.props.history.push("/products");
+        }
+    }
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        if (nextProps.security.validToken) {
+            this.props.history.push("/products");
+        }
+
+        if (nextProps.errors) {
+            this.setState({errors: nextProps.errors});
+        }
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const LoginRequest = {
+            username: this.state.username,
+            password: this.state.password
+        };
+
+        this.props.login(LoginRequest);
+    }
+
+    onChange(e) {
+        this.setState({
+            [e.target.name] : e.target.value
+        });
+    }
+
     render() {
+        const { errors } = this.state;
+
         return (
             <div className="container">
                 <h4 className="card-title mb-4 mt-1">Sign in</h4>
-                <div className="alert alert-danger"
-                     th:if="${param.error}">Invalid username and password.
-                </div>
-                <div className="alert alert-danger"
-                     th:if="${param.logout}">You have been logged out.
-                </div>
-                <form action="/loginAction"
-                      method="post">
+                <form onSubmit = {this.onSubmit}>
                     <div className="form-group">
                         <label htmlFor="inputUsername">Your username:</label>
                         <div className="input-group">
                             <div className="input-group-prepend">
-                                                <span className="input-group-text">
-                                                    <i className="fa fa-user fa-fw" /></span>
+                                <span className="input-group-text">
+                                    <i className="fa fa-user fa-fw" /></span>
                             </div>
                             <input name="username"
                                    id="inputUsername"
-                                   className="form-control"
+                                   className={classnames("form-control", {
+                                       "is-invalid": errors.username
+                                   })}
                                    placeholder="Username"
                                    type="text"
                                    required="required"
-                                   autoFocus="autofocus" />
+                                   value={this.state.username}
+                                   onChange={this.onChange}
+                                   autoFocus />
                         </div>
+                        {errors.username && (
+                            <div className="invalid-feedback">{errors.username}</div>
+                        )}
                     </div>
                     <div className="form-group">
                         <label htmlFor="inputPassword">Your password:</label>
                         <div className="input-group">
                             <div className="input-group-prepend">
-                                                <span className="input-group-text">
-                                                    <i className="fa fa-lock fa-fw" /></span>
+                                <span className="input-group-text">
+                                    <i className="fa fa-lock fa-fw" /></span>
                             </div>
                             <input name="password"
                                    id="inputPassword"
-                                   className="form-control"
+                                   className={classnames("form-control", {
+                                       "is-invalid": errors.password
+                                   })}
                                    placeholder="******"
                                    type="password"
+                                   value={this.state.password}
+                                   onChange={this.onChange}
                                    required="required" />
                         </div>
+                        {errors.password && (
+                            <div className="invalid-feedback">{errors.password}</div>
+                        )}
                     </div>
                     <div className="checkbox">
                         <label htmlFor="remember-me">
@@ -65,4 +122,18 @@ class Login extends Component {
     }
 }
 
-export default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired,
+    security: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    security: state.security,
+    errors: state.errors
+});
+
+export default connect(
+    mapStateToProps,
+    {login}
+)(Login);
