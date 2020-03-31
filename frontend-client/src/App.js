23d1060b93f5da0f8e36151f9d4946336b03c493
zyv42 <zyv42@sdf.org>
2020-03-31 20:07:42 +0300

    updated App.js to properly use bootstrap styles and removed unused imports

--- frontend-client/src/App.js
+++ frontend-client/src/App.js
@@ -2,12 +2,11 @@ import React, {Component} from 'react';
 import './App.css';
 import {Provider} from "react-redux";
 import store from "./store";
+import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Header from "./components/Layout/Header";
 import Landing from "./components/Landing";
 import Footer from "./components/Layout/Footer";
-import Login from "./components/Security/Login";
-import Register from "./components/Security/Register";
 import LoginManagement from "./components/Security/LoginManagement";
 
 class App extends Component {
@@ -33,6 +32,9 @@ class App extends Component {
                         {
                             // Private Routes
                         }
+                        <Switch>
+
+                        </Switch>
                         <Footer />
                     </div>
                 </Router>
