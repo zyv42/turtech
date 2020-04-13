6c2ad2e2439169d6ac04497d1a05d01464dfe10e
zyv42 <zyv42@sdf.org>
2020-04-13 21:23:14 +0300

    add Product.java to the catalog-service module to represent products of the web-store

---
+++ catalog-service/src/main/java/xyz/turtech/catalog/persistence/domain/Product.java
@@ -0,0 +1,177 @@
+package xyz.turtech.catalog.persistence.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String name;
+
+    private String manufacturer;
+
+    private LocalDate manufactureDate;
+
+    private String category;
+
+    private String condition;
+
+    private double shippingWeight;
+
+    private double listPrice;
+
+    private double ourPrice;
+
+    private String description;
+
+    private String specifications;
+
+    private int inStockNumber;
+
+    private boolean discontinued = false;
+
+    /*
+    private MultipartFile product Image;
+     */
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public LocalDate getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(LocalDate manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public double getShippingWeight() {
+        return shippingWeight;
+    }
+
+    public void setShippingWeight(double shippingWeight) {
+        this.shippingWeight = shippingWeight;
+    }
+
+    public double getListPrice() {
+        return listPrice;
+    }
+
+    public void setListPrice(double listPrice) {
+        this.listPrice = listPrice;
+    }
+
+    public double getOurPrice() {
+        return ourPrice;
+    }
+
+    public void setOurPrice(double ourPrice) {
+        this.ourPrice = ourPrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSpecifications() {
+        return specifications;
+    }
+
+    public void setSpecifications(String specifications) {
+        this.specifications = specifications;
+    }
+
+    public int getInStockNumber() {
+        return inStockNumber;
+    }
+
+    public void setInStockNumber(int inStockNumber) {
+        this.inStockNumber = inStockNumber;
+    }
+
+    public boolean isDiscontinued() {
+        return discontinued;
+    }
+
+    public void setDiscontinued(boolean discontinued) {
+        this.discontinued = discontinued;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id.equals(product.id) &&
+                name.equals(product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", manufactureDate=" + manufactureDate +
+                ", category='" + category + '\'' +
+                ", condition='" + condition + '\'' +
+                ", shippingWeight=" + shippingWeight +
+                ", listPrice=" + listPrice +
+                ", ourPrice=" + ourPrice +
+                ", description='" + description + '\'' +
+                ", specifications='" + specifications + '\'' +
+                ", inStockNumber=" + inStockNumber +
+                ", discontinued=" + discontinued +
+                '}';
+    }
+}
