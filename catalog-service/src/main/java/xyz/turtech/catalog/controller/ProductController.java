2ff8dc93d39d238a9d5d582bfa7214731bb7a6f7
zyv42 <zyv42@sdf.org>
2020-04-15 18:00:15 +0300

    update ProductController.java of the catalog-service module

--- catalog-service/src/main/java/xyz/turtech/catalog/controller/ProductController.java
+++ catalog-service/src/main/java/xyz/turtech/catalog/controller/ProductController.java
@@ -1,16 +1,23 @@
 package xyz.turtech.catalog.controller;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.turtech.catalog.persistence.domain.Product;
+import xyz.turtech.catalog.persistence.service.ProductService;
 
 @RestController
 public class ProductController {
 
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping("/all")
-    public Page<Product> getAllProducts() {
-
+    public Page<Product> getAllProducts(PageRequest pageRequest) {
+        return productService.findAll(pageRequest);
     }
 }
