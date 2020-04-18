bd4ad62341754d1b4180ec0fc59727f21eace4e7
zyv42 <zyv42@sdf.org>
2020-04-18 14:47:34 +0300

    update ProductController of the catalog-service module adjusting security

--- catalog-service/src/main/java/xyz/turtech/catalog/controller/ProductController.java
+++ catalog-service/src/main/java/xyz/turtech/catalog/controller/ProductController.java
@@ -2,6 +2,7 @@ package xyz.turtech.catalog.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.turtech.catalog.persistence.domain.Product;
@@ -17,6 +18,7 @@ public class ProductController {
     }
 
     @GetMapping("/all")
+    @PreAuthorize("permitAll()")
     public Page<Product> getAllProducts(PageRequest pageRequest) {
         return productService.findAll(pageRequest);
     }
