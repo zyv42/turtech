1407bc047c19181d52c1ac73c9a5e2224daa5ed2
zyv42 <zyv42@sdf.org>
2020-04-21 17:22:11 +0300

    add getProduct method to the catalog-service module

--- catalog-service/src/main/java/xyz/turtech/catalog/controller/ProductController.java
+++ catalog-service/src/main/java/xyz/turtech/catalog/controller/ProductController.java
@@ -2,9 +2,12 @@ package xyz.turtech.catalog.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.turtech.catalog.persistence.domain.Product;
 import xyz.turtech.catalog.persistence.service.ProductService;
@@ -23,5 +26,14 @@ public class ProductController {
     @CrossOrigin(origins = "http://localhost:3000")
     public Page<Product> getAllProducts() {
         return productService.findAll(PageRequest.of(0, 5));
+    }
+
+    @GetMapping("/{productId}")
+    @PreAuthorize("permitAll()")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<?> getProduct(@PathVariable String productId) {
+        Product product = productService.findOne(productId);
+
+        return new ResponseEntity<>(product, HttpStatus.OK);
     }
 }
