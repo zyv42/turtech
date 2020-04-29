4794f83ab4ef55fc8b646a07a5127030d98bb511
zyv42 <zyv42@sdf.org>
2020-04-29 22:07:09 +0300

    add shopping-cart-service-module to the project

---
+++ shopping-cart-service/src/main/java/xyz/turtech/cart/ShoppingCartApplication.java
@@ -0,0 +1,22 @@
+package xyz.turtech.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableOAuth2Client
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ShoppingCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingCartApplication.class, args);
+    }
+}
