b76cd47f860f7510d40a4ac96117038e69749369
zyv42 <zyv42@sdf.org>
2020-04-30 22:04:54 +0300

    add CartItem and ShoppingCart classes to the shopping-cart-service module

---
+++ shopping-cart-service/src/main/java/xyz/turtech/cart/persistence/domain/CartItem.java
@@ -0,0 +1,82 @@
+package xyz.turtech.cart.persistence.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class CartItem implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private int qty;
+    private BigDecimal subtotal;
+    private String productId;
+    private String shoppingCartId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getShoppingCartId() {
+        return shoppingCartId;
+    }
+
+    public void setShoppingCartId(String shoppingCartId) {
+        this.shoppingCartId = shoppingCartId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CartItem cartItem = (CartItem) o;
+        return id.equals(cartItem.id) &&
+                productId.equals(cartItem.productId) &&
+                shoppingCartId.equals(cartItem.shoppingCartId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, productId, shoppingCartId);
+    }
+
+    @Override
+    public String toString() {
+        return "CartItem{" +
+                "id='" + id + '\'' +
+                ", qty=" + qty +
+                ", subtotal=" + subtotal +
+                ", productId='" + productId + '\'' +
+                ", shoppingCartId='" + shoppingCartId + '\'' +
+                '}';
+    }
+}
---
+++ shopping-cart-service/src/main/java/xyz/turtech/cart/persistence/domain/ShoppingCart.java
@@ -0,0 +1,71 @@
+package xyz.turtech.cart.persistence.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+
+public class ShoppingCart implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private BigDecimal grandTotal;
+    private List<CartItem> cartItemList;
+    private String userId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public BigDecimal getGrandTotal() {
+        return grandTotal;
+    }
+
+    public void setGrandTotal(BigDecimal grandTotal) {
+        this.grandTotal = grandTotal;
+    }
+
+    public List<CartItem> getCartItemList() {
+        return cartItemList;
+    }
+
+    public void setCartItemList(List<CartItem> cartItemList) {
+        this.cartItemList = cartItemList;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ShoppingCart that = (ShoppingCart) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{" +
+                "id='" + id + '\'' +
+                ", grandTotal=" + grandTotal +
+                ", cartItemList=" + cartItemList +
+                ", userId='" + userId + '\'' +
+                '}';
+    }
+}
