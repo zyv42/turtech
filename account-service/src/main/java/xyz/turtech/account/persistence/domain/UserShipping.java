85c57edf4d4544e12e2e3871d0aff3af01da94b5
zyv42 <zyv42@sdf.org>
2020-06-01 17:29:33 +0300

    add UserShipping class to the account service module

---
+++ account-service/src/main/java/xyz/turtech/account/persistence/domain/UserShipping.java
@@ -0,0 +1,124 @@
+package xyz.turtech.account.persistence.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Document
+public class UserShipping implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+    private String userShippingName;
+    private String userShippingStreet1;
+    private String userShippingStreet2;
+    private String userShippingCity;
+    private String userShippingCountry;
+    private String userShippingZipcode;
+    private boolean userShippingDefault;
+    private String userId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserShippingName() {
+        return userShippingName;
+    }
+
+    public void setUserShippingName(String userShippingName) {
+        this.userShippingName = userShippingName;
+    }
+
+    public String getUserShippingStreet1() {
+        return userShippingStreet1;
+    }
+
+    public void setUserShippingStreet1(String userShippingStreet1) {
+        this.userShippingStreet1 = userShippingStreet1;
+    }
+
+    public String getUserShippingStreet2() {
+        return userShippingStreet2;
+    }
+
+    public void setUserShippingStreet2(String userShippingStreet2) {
+        this.userShippingStreet2 = userShippingStreet2;
+    }
+
+    public String getUserShippingCity() {
+        return userShippingCity;
+    }
+
+    public void setUserShippingCity(String userShippingCity) {
+        this.userShippingCity = userShippingCity;
+    }
+
+    public String getUserShippingCountry() {
+        return userShippingCountry;
+    }
+
+    public void setUserShippingCountry(String userShippingCountry) {
+        this.userShippingCountry = userShippingCountry;
+    }
+
+    public String getUserShippingZipcode() {
+        return userShippingZipcode;
+    }
+
+    public void setUserShippingZipcode(String userShippingZipcode) {
+        this.userShippingZipcode = userShippingZipcode;
+    }
+
+    public boolean isUserShippingDefault() {
+        return userShippingDefault;
+    }
+
+    public void setUserShippingDefault(boolean userShippingDefault) {
+        this.userShippingDefault = userShippingDefault;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserShipping that = (UserShipping) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "UserShipping{" +
+                "id='" + id + '\'' +
+                ", userShippingName='" + userShippingName + '\'' +
+                ", userShippingStreet1='" + userShippingStreet1 + '\'' +
+                ", userShippingStreet2='" + userShippingStreet2 + '\'' +
+                ", userShippingCity='" + userShippingCity + '\'' +
+                ", userShippingCountry='" + userShippingCountry + '\'' +
+                ", userShippingZipcode='" + userShippingZipcode + '\'' +
+                ", userShippingDefault=" + userShippingDefault +
+                ", userId='" + userId + '\'' +
+                '}';
+    }
+}
