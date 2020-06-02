5e9b3f2aa2a47708b5e05fec45f509df7a8c2fba
zyv42 <zyv42@sdf.org>
2020-06-02 20:47:15 +0300

    add Order class to the order service module

---
+++ order-service/src/main/java/xyz/turtech/order/persistence/domain/BillingAddress.java
@@ -0,0 +1,11 @@
+package xyz.turtech.order.persistence.domain;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Document
+public class BillingAddress implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ order-service/src/main/java/xyz/turtech/order/persistence/domain/Order.java
@@ -0,0 +1,148 @@
+package xyz.turtech.order.persistence.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+@Document
+public class Order implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+    private LocalDateTime orderDate;
+    private LocalDateTime shippingDate;
+    private String shippingMethod;
+    private String orderStatus;
+    private BigDecimal orderTotal;
+    private List<CartItem> cartItems;
+    private String shippingAddressId;
+    private String billingAddressId;
+    private String paymentId;
+    private String userId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public LocalDateTime getShippingDate() {
+        return shippingDate;
+    }
+
+    public void setShippingDate(LocalDateTime shippingDate) {
+        this.shippingDate = shippingDate;
+    }
+
+    public String getShippingMethod() {
+        return shippingMethod;
+    }
+
+    public void setShippingMethod(String shippingMethod) {
+        this.shippingMethod = shippingMethod;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public BigDecimal getOrderTotal() {
+        return orderTotal;
+    }
+
+    public void setOrderTotal(BigDecimal orderTotal) {
+        this.orderTotal = orderTotal;
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    public String getShippingAddressId() {
+        return shippingAddressId;
+    }
+
+    public void setShippingAddressId(String shippingAddressId) {
+        this.shippingAddressId = shippingAddressId;
+    }
+
+    public String getBillingAddressId() {
+        return billingAddressId;
+    }
+
+    public void setBillingAddressId(String billingAddressId) {
+        this.billingAddressId = billingAddressId;
+    }
+
+    public String getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(String paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return id.equals(order.id) &&
+                userId.equals(order.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id='" + id + '\'' +
+                ", orderDate=" + orderDate +
+                ", shippingDate=" + shippingDate +
+                ", shippingMethod='" + shippingMethod + '\'' +
+                ", orderStatus='" + orderStatus + '\'' +
+                ", orderTotal=" + orderTotal +
+                ", cartItems=" + cartItems +
+                ", shippingAddressId='" + shippingAddressId + '\'' +
+                ", billingAddressId='" + billingAddressId + '\'' +
+                ", paymentId='" + paymentId + '\'' +
+                ", userId='" + userId + '\'' +
+                '}';
+    }
+}
