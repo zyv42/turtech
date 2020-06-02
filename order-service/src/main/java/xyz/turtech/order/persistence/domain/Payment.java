7651718019046526e3b2928de1bc52b366fbc452
zyv42 <zyv42@sdf.org>
2020-06-02 22:01:32 +0300

    add Payment class to the order service module

--- order-service/src/main/java/xyz/turtech/order/persistence/domain/Payment.java
+++ order-service/src/main/java/xyz/turtech/order/persistence/domain/Payment.java
@@ -4,6 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Document
 public class Payment implements Serializable {
@@ -12,5 +13,122 @@ public class Payment implements Serializable {
 
     @Id
     private String id;
+    private String type;
+    private String cardName;
+    private String cardNumber;
+    private int expiryMonth;
+    private int expiryYear;
+    private int cvc;
+    private String holderName;
+    private String orderId;
+    private String BillingAddressId;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public int getExpiryMonth() {
+        return expiryMonth;
+    }
+
+    public void setExpiryMonth(int expiryMonth) {
+        this.expiryMonth = expiryMonth;
+    }
+
+    public int getExpiryYear() {
+        return expiryYear;
+    }
+
+    public void setExpiryYear(int expiryYear) {
+        this.expiryYear = expiryYear;
+    }
+
+    public int getCvc() {
+        return cvc;
+    }
+
+    public void setCvc(int cvc) {
+        this.cvc = cvc;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public void setHolderName(String holderName) {
+        this.holderName = holderName;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getBillingAddressId() {
+        return BillingAddressId;
+    }
+
+    public void setBillingAddressId(String billingAddressId) {
+        BillingAddressId = billingAddressId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Payment payment = (Payment) o;
+        return id.equals(payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "id='" + id + '\'' +
+                ", type='" + type + '\'' +
+                ", cardName='" + cardName + '\'' +
+                ", cardNumber='" + cardNumber + '\'' +
+                ", expiryMonth=" + expiryMonth +
+                ", expiryYear=" + expiryYear +
+                ", cvc=" + cvc +
+                ", holderName='" + holderName + '\'' +
+                ", orderId='" + orderId + '\'' +
+                ", BillingAddressId='" + BillingAddressId + '\'' +
+                '}';
+    }
 }
