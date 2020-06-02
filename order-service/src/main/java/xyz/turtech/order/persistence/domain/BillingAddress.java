04b8751eaac0f638587ad5f461f3fc356f305c24
zyv42 <zyv42@sdf.org>
2020-06-02 21:52:39 +0300

    add BillingAddress class to the order service module

--- order-service/src/main/java/xyz/turtech/order/persistence/domain/BillingAddress.java
+++ order-service/src/main/java/xyz/turtech/order/persistence/domain/BillingAddress.java
@@ -1,11 +1,114 @@
 package xyz.turtech.order.persistence.domain;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Document
 public class BillingAddress implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+    private String billingAddressName;
+    private String billingAddressStreet1;
+    private String billingAddressStreet2;
+    private String billingAddressCity;
+    private String billingAddressCountry;
+    private String billingAddressZipcode;
+    private String orderId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBillingAddressName() {
+        return billingAddressName;
+    }
+
+    public void setBillingAddressName(String billingAddressName) {
+        this.billingAddressName = billingAddressName;
+    }
+
+    public String getBillingAddressStreet1() {
+        return billingAddressStreet1;
+    }
+
+    public void setBillingAddressStreet1(String billingAddressStreet1) {
+        this.billingAddressStreet1 = billingAddressStreet1;
+    }
+
+    public String getBillingAddressStreet2() {
+        return billingAddressStreet2;
+    }
+
+    public void setBillingAddressStreet2(String billingAddressStreet2) {
+        this.billingAddressStreet2 = billingAddressStreet2;
+    }
+
+    public String getBillingAddressCity() {
+        return billingAddressCity;
+    }
+
+    public void setBillingAddressCity(String billingAddressCity) {
+        this.billingAddressCity = billingAddressCity;
+    }
+
+    public String getBillingAddressCountry() {
+        return billingAddressCountry;
+    }
+
+    public void setBillingAddressCountry(String billingAddressCountry) {
+        this.billingAddressCountry = billingAddressCountry;
+    }
+
+    public String getBillingAddressZipcode() {
+        return billingAddressZipcode;
+    }
+
+    public void setBillingAddressZipcode(String billingAddressZipcode) {
+        this.billingAddressZipcode = billingAddressZipcode;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BillingAddress that = (BillingAddress) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "BillingAddress{" +
+                "id='" + id + '\'' +
+                ", billingAddressName='" + billingAddressName + '\'' +
+                ", billingAddressStreet1='" + billingAddressStreet1 + '\'' +
+                ", billingAddressStreet2='" + billingAddressStreet2 + '\'' +
+                ", billingAddressCity='" + billingAddressCity + '\'' +
+                ", billingAddressCountry='" + billingAddressCountry + '\'' +
+                ", billingAddressZipcode='" + billingAddressZipcode + '\'' +
+                ", orderId='" + orderId + '\'' +
+                '}';
+    }
 }
