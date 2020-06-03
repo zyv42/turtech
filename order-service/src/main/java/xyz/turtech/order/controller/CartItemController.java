e7adbb85f69b0532e4374e55fd23017407981798
zyv42 <zyv42@sdf.org>
2020-06-03 20:45:55 +0300

    add OrderController and CartItemController to the order service module

---
+++ order-service/src/main/java/xyz/turtech/order/controller/CartItemController.java
@@ -0,0 +1,30 @@
+package xyz.turtech.order.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.turtech.order.persistence.domain.CartItem;
+import xyz.turtech.order.persistence.service.CartItemService;
+
+@RestController
+public class CartItemController {
+
+    private final CartItemService cartItemService;
+
+    public CartItemController(CartItemService cartItemService) {
+        this.cartItemService = cartItemService;
+    }
+
+    @GetMapping("/cartItemsByOrderId")
+    @PreAuthorize("permitAll()")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<?> getCartItemsByOrderId(@PathVariable String orderId) {
+        Iterable<CartItem> cartItems = cartItemService.findByOrderId(orderId);
+
+        return new ResponseEntity<>(cartItems, HttpStatus.OK);
+    }
+}
---
+++ order-service/src/main/java/xyz/turtech/order/controller/OrderController.java
@@ -0,0 +1,30 @@
+package xyz.turtech.order.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.turtech.order.persistence.domain.Order;
+import xyz.turtech.order.persistence.service.OrderService;
+
+@RestController
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/orders")
+    @PreAuthorize("permitAll()")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<?> getOrdersByUserId(@PathVariable String userId) {
+        Iterable<Order> orders = orderService.findByUserId(userId);
+
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+}
