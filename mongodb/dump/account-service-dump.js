815cc1921d9c9b7d516d0b3f68f18bcc49913786
zyv42 <zyv42@sdf.org>
2020-07-09 20:36:42 +0300

    update account-service-dump.js for the mongodb database

--- mongodb/dump/account-service-dump.js
+++ mongodb/dump/account-service-dump.js
@@ -8,6 +8,13 @@ db.users.update(
     { "_id": "demo" },
     {
         // here goes information about demo account
+        "_id": "demo",
+        "username": "demo_user",
+        "password": "password",
+        "firstName": "John",
+        "lastName": "Smith",
+        "email": "demo@email.com",
+        "phone": "+380998888888"
     },
     {
         upsert: true
