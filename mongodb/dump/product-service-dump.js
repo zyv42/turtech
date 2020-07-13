f83f778023ebb6cd4e5ac1166ff3c2a36af6d478
zyv42 <zyv42@sdf.org>
2020-07-13 19:29:04 +0300

    update product-service-dump.js for the mongodb(database) module

--- mongodb/dump/product-service-dump.js
+++ mongodb/dump/product-service-dump.js
@@ -8,7 +8,19 @@ db.products.update(
     { "_id": "demo" },
     {
         // here goes information about demo products
-
+        "_id": "demo",
+        "name": "",
+        "manufacturer": "",
+        "manufactureDate": "",
+        "category": "",
+        "condition": "",
+        "shippingWeight": "",
+        "listPrice": "",
+        "ourPrice": "",
+        "description": "",
+        "specifications": "",
+        "inStockNumber": "",
+        "discontinued": "",
     },
     {
         upsert: true
